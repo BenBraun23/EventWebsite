626a1df58c67c549a6a76f4f04938f86467de958
Ben <benjamindbraun@gmail.com>
2021-02-28 19:12:33 -0500

    hello world

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const PORT = 3000;
+const mysql = require("mysql");
+
+var con = mysql.createConnection({
+	host:"localhost",
+	user:"EventWebsite",
+	password:"EventWebsite1!"
+});
+
+con.connect(function(err){
+	if(err) throw err;
+	console.log("Connected!");
+});
 
 const app = express();
 
 app.get("/", function(request, response) {
 	response.send("Hello World!");
-})
+});
 
 app.listen(PORT, function() {
 	console.log(`Server listening on port ${PORT}`);
-})
+});
